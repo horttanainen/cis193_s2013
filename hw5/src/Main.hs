3ba241d14e19f492e2c7e1e63bb7efa786dbf923
Santeri Horttanainen <santeri.horttanainen@gmail.com>
2017-05-20 19:12:35 +0300

    ex4 done

--- hw5/src/Main.hs
+++ hw5/src/Main.hs
@@ -21,8 +21,39 @@ instance Expr ExprT where
 	add = Add
 	mul = Mul
 
+instance Expr Integer where
+	lit = id
+	add = (+)
+	mul = (*)
 
+instance Expr Bool where
+	lit x
+		| x <= 0 	= False
+		| otherwise 	= True
+	add = (||)
+	mul = (&&)
 
+newtype MinMax = MinMax Integer deriving (Eq, Show)
+
+instance Expr MinMax where
+	lit = MinMax
+	add (MinMax x) (MinMax y) = MinMax (max x y)
+	mul (MinMax x) (MinMax y) = MinMax (min x y)
+
+newtype Mod7 = Mod7 Integer deriving (Eq, Show)
+
+instance Expr Mod7 where
+	lit x = Mod7 (mod x 7)
+	add (Mod7 x) (Mod7 y) = Mod7 (mod (x + y) 7)
+	mul (Mod7 x) (Mod7 y) = Mod7 (mod (x * y) 7)
+
+testExp :: Expr a => Maybe a
+testExp = parseExp lit add mul "(3 * -4) + 5"
+
+testInteger = testExp :: Maybe Integer
+testBool = testExp :: Maybe Bool
+testMM = testExp :: Maybe MinMax
+testSat = testExp :: Maybe Mod7
 
 main :: IO ()
 main = do
