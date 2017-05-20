94765d2af71a5a802fd9ee3ad1322cf2cb909c91
Santeri Horttanainen <santeri.horttanainen@gmail.com>
2017-05-20 17:59:04 +0300

    ex2 done

--- hw5/src/Main.hs
+++ hw5/src/Main.hs
@@ -1,11 +1,18 @@
 module Main where
 
 import ExprT
+import Parser
 
 eval :: ExprT -> Integer
 eval (Lit n) = n
 eval (Add e1 e2) = (eval e1) + (eval e2)
 eval (Mul e1 e2) = (eval e1) * (eval e2)
+
+
+
+evalStr :: String -> Maybe Integer
+evalStr str = fmap eval $ parseExp Lit Add Mul str
+
 
 main :: IO ()
 main = do
