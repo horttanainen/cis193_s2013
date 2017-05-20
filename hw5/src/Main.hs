d02e65ae5902926303bf463519e6b24d30fa62c3
Santeri Horttanainen <santeri.horttanainen@gmail.com>
2017-05-20 17:49:07 +0300

    ex1 done

--- hw5/src/Main.hs
+++ hw5/src/Main.hs
@@ -1,5 +1,12 @@
 module Main where
 
+import ExprT
+
+eval :: ExprT -> Integer
+eval (Lit n) = n
+eval (Add e1 e2) = (eval e1) + (eval e2)
+eval (Mul e1 e2) = (eval e1) * (eval e2)
+
 main :: IO ()
 main = do
   putStrLn "hello world"
