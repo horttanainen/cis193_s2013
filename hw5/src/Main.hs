0cde526d2e61c616303a5e8ed9519951cc2a31e0
Santeri Horttanainen <santeri.horttanainen@gmail.com>
2017-05-20 18:21:39 +0300

    ex3 done

--- hw5/src/Main.hs
+++ hw5/src/Main.hs
@@ -8,10 +8,20 @@ eval (Lit n) = n
 eval (Add e1 e2) = (eval e1) + (eval e2)
 eval (Mul e1 e2) = (eval e1) * (eval e2)
 
-
-
 evalStr :: String -> Maybe Integer
 evalStr str = fmap eval $ parseExp Lit Add Mul str
+
+class Expr a where
+	lit :: Integer -> a
+	mul :: a -> a -> a
+	add :: a -> a -> a
+
+instance Expr ExprT where
+	lit = Lit
+	add = Add
+	mul = Mul
+
+
 
 
 main :: IO ()
