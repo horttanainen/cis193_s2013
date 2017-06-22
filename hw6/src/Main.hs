0c1a6c6c65a571c9c2a8949b0756886abe61fa9b
Santeri Horttanainen <santeri.horttanainen@gmail.com>
2017-06-22 16:10:24 +0300

    hw6 ex3 done

--- hw6/src/Main.hs
+++ hw6/src/Main.hs
@@ -12,6 +12,14 @@ fibs2 :: [Integer]
 fibs2 = fibTmp 0 1 where
     fibTmp a b = a : fibTmp b (a + b)
 
+data Stream a = Cons a (Stream a) 
+
+instance Show a => Show (Stream a) where
+    show = show . take 20 . streamToList
+
+streamToList :: Stream a -> [a]
+streamToList (Cons a rest) = a : streamToList rest
+
 main :: IO ()
 main = do
   putStrLn "hello world"
