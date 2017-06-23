1e20bb5270a49aaae3aa2a585469c02a53a8131e
Santeri Horttanainen <santeri.horttanainen@gmail.com>
2017-06-24 00:14:52 +0300

    hw7 ex2 done

--- hw7/src/JoinList.hs
+++ hw7/src/JoinList.hs
@@ -1,6 +1,7 @@
 module JoinList where
 
 import Data.Monoid
+import Sized
 
 data JoinList m a = Empty
     | Single m a
@@ -14,3 +15,17 @@ tag (Append m _ _)  = m
 
 (+++) :: Monoid m => JoinList m a -> JoinList m a -> JoinList m a
 (+++) l1 l2 = Append ((tag l1) <> (tag l2)) l1 l2
+
+indexJ :: (Sized b, Monoid b) => Int -> JoinList b a -> Maybe a
+indexJ _ Empty                              = Nothing
+indexJ i _ | i < 0                          = Nothing
+indexJ i (Single _ a)
+    | i == 0            = Just a
+    | otherwise         = Nothing
+indexJ i (Append b left right)
+    | i > wholeSize             = Nothing
+    | i <= leftSize             = indexJ i left
+    | otherwise                 = indexJ (i - leftSize) right
+    where
+        wholeSize   = getSize $ size b
+        leftSize    = getSize $ size $ tag left
