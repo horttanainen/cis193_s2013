8e8516457885e84e8e149a7cffeeaf57650d9ebd
Santeri Horttanainen <santeri.horttanainen@gmail.com>
2017-06-23 22:34:50 +0300

    hw7 ex1 done

---
+++ hw7/src/JoinList.hs
@@ -0,0 +1,12 @@
+data JoinList m a = Empty
+    | Single m a
+    | Append m (JoinList m a) (JoinList m a)
+deriving (Eq, Show)
+
+tag :: Monoid m => JoinList m a -> m
+tag Empty           = mempty
+tag (Single m _)    = m
+tag (Append m _ _)  = m
+
+(+++) :: Monoid m => JoinList m a -> JoinList m a -> JoinList m a
+(+++) l1 l2 = Append <> (tag l1) (tag l2) l1 l2
