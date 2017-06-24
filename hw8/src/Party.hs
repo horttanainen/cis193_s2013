a1797c10a818a13d44144e51c77de1102e9e69c7
Santeri Horttanainen <santeri.horttanainen@gmail.com>
2017-06-24 22:33:09 +0300

    hw8 ex1 finished

--- hw8/src/Party.hs
+++ hw8/src/Party.hs
@@ -2,3 +2,14 @@ module Party where
 
 import Employee
 
+glCons :: Employee -> GuestList -> GuestList
+glCons e@Emp { empFun = ef} (GL gl fun) = GL (e:gl) (fun + ef)
+
+instance Monoid GuestList where
+    mempty   = GL [] 0
+    mappend (GL gl1 f1) (GL gl2 f2) = GL (gl1 ++ gl2) (f1 + f2)
+
+moreFun :: GuestList -> GuestList -> GuestList
+moreFun gl1@(GL _ f1) gl2@(GL _ f2)
+    | f1 > f2   = gl1
+    | otherwise = gl2
